9b390353b28b7fdec51a17fd54789fe8dbd55bfe
Christopher Raheb <craheb123@gmail.com>
2025-10-04 22:42:06 -0700

    testing: removed paperclip

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+//import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+// <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
   <button class="main button" type="button">Click Me</button>
 `;
