fcceaac3ad51d9c6ea244331cb1ac75c3f997908
Christopher Raheb <craheb123@gmail.com>
2025-10-05 00:10:18 -0700

    step 2 complete: added counter on screen, added click functionality, centered both elements on screen.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
-import exampleIconUrl from "./sprites/egg.png";
+import eggSprite from "./sprites/egg.png";
 import "./style.css";
 
-// <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+// main currency
+let xp: number = 0;
+
 document.body.innerHTML = `
-  <button class="monster" type="button"><img src="${exampleIconUrl}" class="icon" /></button>
+  <div class="monster-flexbox">
+    <div class="monster-wrapper">
+      <p id="xp">XP: <span id="counter">0</span></p>
+      <button id="monster" type="button"><img src="${eggSprite}" class="icon" /></button>
+    </div>
+  </div>
 `;
+
+const monster = document.getElementById("monster")!;
+const counterElement = document.getElementById("counter")!;
+
+monster.addEventListener("click", () => {
+  xp += 1;
+  counterElement.innerHTML = String(xp);
+});
