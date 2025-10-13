8ec55ef29e1953afb2087d9f5ce28a1a68fcf9bf
Christopher Raheb <craheb123@gmail.com>
2025-10-12 21:49:37 -0700

    step 6 complete: added growth_rate text on screen and counters for each upgrade type.

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,9 @@ class Upgrade {
   cost: number;
   growth_rate: number;
   amount: number = 0;
-  button: HTMLButtonElement | null = null;
+
+  buttonElement: HTMLButtonElement | null = null;
+  amountElement: HTMLSpanElement | null = null;
 
   constructor(name: string, cost: number, growth_rate: number) {
     this.name = name;
@@ -22,8 +24,9 @@ class Upgrade {
   public buy() {
     xp -= this.cost;
     this.amount++;
-    this.cost *= 1.2;
-    this.button!.innerHTML = `Buy ${this.name}: ${Math.floor(this.cost)}`;
+    this.cost = Math.floor(this.cost * 1.2);
+    this.buttonElement!.innerHTML = `Buy ${this.name}: ${this.cost}`;
+    if (this.amountElement) this.amountElement.textContent = `x${this.amount}`;
   }
 }
 
@@ -36,24 +39,47 @@ document.body.innerHTML = `
     <main class="monster-flexbox content">
       <div class="monster-wrapper">
         <p id="xp">XP: <span id="counter">0</span></p>
+        <p id="growth-rate">Growth rate: ${growth_rate} xp/s</p>
         <button id="monster-button" type="button"><img src="${eggSprite}" class="icon" /></button>
       </div>
     </main>
 
     <aside class="sidebar">
-      <button class="upgrade" id="A">Buy A: ${UPGRADES[0].cost}</button>
-      <button class="upgrade" id="B">Buy B: ${UPGRADES[1].cost}</button>
-      <button class="upgrade" id="C">Buy C: ${UPGRADES[2].cost}</button>
+      <div class="upgrade-wrapper">
+        <span class="upgrade-amount" id="A-amount">x0</span>
+        <button class="upgrade" id="A">Buy A: ${UPGRADES[0].cost}</button>
+      </div>
+      <div class="upgrade-wrapper">
+        <span class="upgrade-amount" id="B-amount">x0</span>
+        <button class="upgrade" id="B">Buy B: ${UPGRADES[1].cost}</button>
+      </div>
+      <div class="upgrade-wrapper">
+        <span class="upgrade-amount" id="C-amount">x0</span>
+        <button class="upgrade" id="C">Buy C: ${UPGRADES[2].cost}</button>
+      </div>
     </aside>
   </div>
 `;
 
 const MONSTER = document.getElementById("monster-button")! as HTMLButtonElement;
 const COUNTER_ELEMENT = document.getElementById("counter")! as HTMLSpanElement;
+const GROWTH_RATE_ELEMENT = document.getElementById(
+  "growth-rate",
+)! as HTMLSpanElement;
 
-UPGRADES[0].button = document.getElementById("A")! as HTMLButtonElement;
-UPGRADES[1].button = document.getElementById("B")! as HTMLButtonElement;
-UPGRADES[2].button = document.getElementById("C")! as HTMLButtonElement;
+UPGRADES[0].buttonElement = document.getElementById("A")! as HTMLButtonElement;
+UPGRADES[1].buttonElement = document.getElementById("B")! as HTMLButtonElement;
+UPGRADES[2].buttonElement = document.getElementById("C")! as HTMLButtonElement;
+
+UPGRADES[0].amountElement = document.getElementById(
+  "A-amount",
+)! as HTMLSpanElement;
+UPGRADES[1].amountElement = document.getElementById(
+  "B-amount",
+)! as HTMLSpanElement;
+UPGRADES[2].amountElement = document.getElementById(
+  "C-amount",
+)! as HTMLSpanElement;
 
 requestAnimationFrame(autoClick);
 
@@ -63,7 +89,7 @@ MONSTER.addEventListener("click", () => {
 });
 
 for (const UPGRADE of UPGRADES) {
-  UPGRADE.button!.addEventListener("click", () => {
+  UPGRADE.buttonElement!.addEventListener("click", () => {
     UPGRADE.buy();
   });
 }
@@ -73,10 +99,12 @@ function autoClick(timestamp: DOMHighResTimeStamp) {
   growth_rate = 0;
   for (const UPGRADE of UPGRADES) {
     if (xp >= UPGRADE.cost) {
-      UPGRADE.button!.disabled = false;
+      UPGRADE.buttonElement!.disabled = false;
     } else {
-      UPGRADE.button!.disabled = true;
+      UPGRADE.buttonElement!.disabled = true;
     }
+
+    UPGRADE.amountElement!.textContent = `x${UPGRADE.amount}`;
 
     growth_rate += UPGRADE.growth_rate * UPGRADE.amount;
   }
@@ -85,7 +113,14 @@ function autoClick(timestamp: DOMHighResTimeStamp) {
   const DELTA_SECONDS = (timestamp - lastTimestamp) / 1000;
   xp += DELTA_SECONDS * growth_rate;
   COUNTER_ELEMENT.innerHTML = xp.toFixed(2);
+  GROWTH_RATE_ELEMENT.innerHTML = `Growth rate: ${round(growth_rate, 1)} xp/s`;
 
   lastTimestamp = timestamp;
   requestAnimationFrame(autoClick);
+}
+
+// https://stackoverflow.com/a/7343013
+function round(value: number, precision: number) {
+  const MULTIPLIER = Math.pow(10, precision || 0);
+  return Math.round(value * MULTIPLIER) / MULTIPLIER;
 }
