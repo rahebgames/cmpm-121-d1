c49fbaa63973a571c23f116f60a83e3fd38d0c98
Christopher Raheb <craheb123@gmail.com>
2025-10-13 14:00:14 -0700

    Step 8 complete: renamed upgrades, hanged growth rate text, and removed decimals.

--- src/main.ts
+++ src/main.ts
@@ -24,38 +24,44 @@ class Upgrade {
   public buy() {
     xp -= this.cost;
     this.amount++;
-    this.cost = round(this.cost * 1.15, 2);
-    this.buttonElement!.innerHTML = `Buy ${this.name}: ${this.cost}`;
+    this.cost = Math.floor(this.cost * 1.15);
+    this.buttonElement!.innerHTML = `Hire ${this.name}: ${this.cost}`;
     this.amountElement!.textContent = `x${this.amount}`;
   }
 }
 
-UPGRADES.push(new Upgrade("A", 10, 0.1));
-UPGRADES.push(new Upgrade("B", 100, 2.0));
-UPGRADES.push(new Upgrade("C", 1000, 50));
+UPGRADES.push(new Upgrade("Boxer", 10, 0.1));
+UPGRADES.push(new Upgrade("Swordsman", 100, 2.0));
+UPGRADES.push(new Upgrade("Archer", 1000, 50));
 
 document.body.innerHTML = `
   <div class="app">
     <main class="monster-flexbox content">
       <div class="monster-wrapper">
         <p id="xp">XP: <span id="counter">0</span></p>
-        <p id="growth-rate">Growth rate: ${growth_rate} xp/s</p>
+        <p id="growth-rate">per second: ${growth_rate}</p>
         <button id="monster-button" type="button"><img src="${eggSprite}" class="icon" /></button>
       </div>
     </main>
 
     <aside class="sidebar">
       <div class="upgrade-wrapper">
-        <span class="upgrade-amount" id="A-amount">x0</span>
-        <button class="upgrade" id="A">Buy A: ${UPGRADES[0].cost}</button>
+        <span class="upgrade-amount" id="boxer-amount">x0</span>
+        <button class="upgrade" id="boxer">Hire Boxer: ${
+  UPGRADES[0].cost
+}</button>
       </div>
       <div class="upgrade-wrapper">
-        <span class="upgrade-amount" id="B-amount">x0</span>
-        <button class="upgrade" id="B">Buy B: ${UPGRADES[1].cost}</button>
+        <span class="upgrade-amount" id="swordsman-amount">x0</span>
+        <button class="upgrade" id="swordsman">Hire Swordsman: ${
+  UPGRADES[1].cost
+}</button>
       </div>
       <div class="upgrade-wrapper">
-        <span class="upgrade-amount" id="C-amount">x0</span>
-        <button class="upgrade" id="C">Buy C: ${UPGRADES[2].cost}</button>
+        <span class="upgrade-amount" id="archer-amount">x0</span>
+        <button class="upgrade" id="archer">Hire Archer: ${
+  UPGRADES[2].cost
+}</button>
       </div>
     </aside>
   </div>
@@ -67,18 +73,24 @@ const GROWTH_RATE_ELEMENT = document.getElementById(
   "growth-rate",
 )! as HTMLSpanElement;
 
-UPGRADES[0].buttonElement = document.getElementById("A")! as HTMLButtonElement;
-UPGRADES[1].buttonElement = document.getElementById("B")! as HTMLButtonElement;
-UPGRADES[2].buttonElement = document.getElementById("C")! as HTMLButtonElement;
+UPGRADES[0].buttonElement = document.getElementById(
+  "boxer",
+)! as HTMLButtonElement;
+UPGRADES[1].buttonElement = document.getElementById(
+  "swordsman",
+)! as HTMLButtonElement;
+UPGRADES[2].buttonElement = document.getElementById(
+  "archer",
+)! as HTMLButtonElement;
 
 UPGRADES[0].amountElement = document.getElementById(
-  "A-amount",
+  "boxer-amount",
 )! as HTMLSpanElement;
 UPGRADES[1].amountElement = document.getElementById(
-  "B-amount",
+  "swordsman-amount",
 )! as HTMLSpanElement;
 UPGRADES[2].amountElement = document.getElementById(
-  "C-amount",
+  "archer-amount",
 )! as HTMLSpanElement;
 
 requestAnimationFrame(autoClick);
@@ -112,8 +124,8 @@ function autoClick(timestamp: DOMHighResTimeStamp) {
   if (lastTimestamp == null) lastTimestamp = timestamp;
   const DELTA_SECONDS = (timestamp - lastTimestamp) / 1000;
   xp += DELTA_SECONDS * growth_rate;
-  COUNTER_ELEMENT.innerHTML = xp.toFixed(2);
-  GROWTH_RATE_ELEMENT.innerHTML = `Growth rate: ${round(growth_rate, 1)} xp/s`;
+  COUNTER_ELEMENT.innerHTML = String(Math.floor(xp));
+  GROWTH_RATE_ELEMENT.innerHTML = `per second: ${round(growth_rate, 1)}`;
 
   lastTimestamp = timestamp;
   requestAnimationFrame(autoClick);
