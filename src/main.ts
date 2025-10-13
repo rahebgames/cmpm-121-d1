752169fb32b3c8f72d12a4e544146c716374bc83
Christopher Raheb <craheb123@gmail.com>
2025-10-12 20:57:54 -0700

    changes: - added sidebar for upgrade buttons - added class for upgrades - added 2 more upgrades and changed all values to fit instructions - made all upgrades affect growth rate

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,57 @@ import "./style.css";
 
 // main currency
 let xp: number = 0;
-let growth_rate = 0;
-let upgrade_cost = 10;
+let growth_rate: number = 0;
+const UPGRADES: Upgrade[] = [];
+
+class Upgrade {
+  name: string;
+  cost: number;
+  growth_rate: number;
+  amount: number = 0;
+  button: HTMLButtonElement | null = null;
+
+  constructor(name: string, cost: number, growth_rate: number) {
+    this.name = name;
+    this.cost = cost;
+    this.growth_rate = growth_rate;
+  }
+
+  public buy() {
+    xp -= this.cost;
+    this.amount++;
+    this.cost *= 1.2;
+    this.button!.innerHTML = `Buy ${this.name}: ${Math.floor(this.cost)}`;
+  }
+}
+
+UPGRADES.push(new Upgrade("A", 10, 0.1));
+UPGRADES.push(new Upgrade("B", 100, 2.0));
+UPGRADES.push(new Upgrade("C", 1000, 50));
 
 document.body.innerHTML = `
-  <div class="monster-flexbox">
-    <div class="monster-wrapper">
-      <p id="xp">XP: <span id="counter">0</span></p>
-      <button id="monster-button" type="button"><img src="${eggSprite}" class="icon" /></button>
-      <button id="upgrade">Buy Auto Clicker: ${upgrade_cost}</button>
-    </div>
+  <div class="app">
+    <main class="monster-flexbox content">
+      <div class="monster-wrapper">
+        <p id="xp">XP: <span id="counter">0</span></p>
+        <button id="monster-button" type="button"><img src="${eggSprite}" class="icon" /></button>
+      </div>
+    </main>
+
+    <aside class="sidebar">
+      <button class="upgrade" id="A">Buy A: ${UPGRADES[0].cost}</button>
+      <button class="upgrade" id="B">Buy B: ${UPGRADES[1].cost}</button>
+      <button class="upgrade" id="C">Buy C: ${UPGRADES[2].cost}</button>
+    </aside>
   </div>
 `;
 
 const MONSTER = document.getElementById("monster-button")! as HTMLButtonElement;
 const COUNTER_ELEMENT = document.getElementById("counter")! as HTMLSpanElement;
-const UPGRADE_BUTTON = document.getElementById("upgrade")! as HTMLButtonElement;
 
-UPGRADE_BUTTON.disabled = true;
+UPGRADES[0].button = document.getElementById("A")! as HTMLButtonElement;
+UPGRADES[1].button = document.getElementById("B")! as HTMLButtonElement;
+UPGRADES[2].button = document.getElementById("C")! as HTMLButtonElement;
 
 requestAnimationFrame(autoClick);
 
@@ -29,27 +62,29 @@ MONSTER.addEventListener("click", () => {
   COUNTER_ELEMENT.innerHTML = String(xp);
 });
 
-UPGRADE_BUTTON.addEventListener("click", () => {
-  xp -= upgrade_cost;
-  upgrade_cost *= 1.2;
-  growth_rate++;
-  UPGRADE_BUTTON.innerHTML = "Buy Auto Clicker: " +
-    String(Math.floor(upgrade_cost));
-});
+for (const UPGRADE of UPGRADES) {
+  UPGRADE.button!.addEventListener("click", () => {
+    UPGRADE.buy();
+  });
+}
 
 let lastTimestamp: number | null = null;
 function autoClick(timestamp: DOMHighResTimeStamp) {
-  if (lastTimestamp == null) lastTimestamp = timestamp;
+  growth_rate = 0;
+  for (const UPGRADE of UPGRADES) {
+    if (xp >= UPGRADE.cost) {
+      UPGRADE.button!.disabled = false;
+    } else {
+      UPGRADE.button!.disabled = true;
+    }
 
+    growth_rate += UPGRADE.growth_rate * UPGRADE.amount;
+  }
+
+  if (lastTimestamp == null) lastTimestamp = timestamp;
   const DELTA_SECONDS = (timestamp - lastTimestamp) / 1000;
   xp += DELTA_SECONDS * growth_rate;
   COUNTER_ELEMENT.innerHTML = xp.toFixed(2);
-
-  if (xp >= upgrade_cost) {
-    UPGRADE_BUTTON.disabled = false;
-  } else {
-    UPGRADE_BUTTON.disabled = true;
-  }
 
   lastTimestamp = timestamp;
   requestAnimationFrame(autoClick);
